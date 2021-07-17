58945b0ba1bfa9c785fdbcdd4053c951527ce200
Jason Chen <jason.chen@shopify.com>
2021-07-17 12:48:04 -0400

    Supports failure tests and Parallel tests

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # SimpleMapReduce
 A simple version of MapReduce written in Go.
 
-# Sequential
+# Sequential Tests
 `go test '-run=TestSequential*' /Users/jchome/src/github.com/drinkbeer/src/SimpleMapReduce/src/mapreduce`
 
-# Parallel
+# Parallel Tests
 `go test '-run=TestParallel*' /Users/jchome/src/github.com/drinkbeer/src/SimpleMapReduce/src/mapreduce`
+
+# All Tests
+`go test /Users/jchome/src/github.com/drinkbeer/src/SimpleMapReduce/src/mapreduce`
+
--- src/mapreduce/distributed_test.go
+++ src/mapreduce/distributed_test.go
@@ -3,15 +3,71 @@ package mapreduce
 import (
 	"strconv"
 	"testing"
+	"time"
 )
 
 func TestParallelBasic(t *testing.T) {
 	mr := SetUpParallel()
 	for i := 0; i < 2; i++ {
-		go RunWorker(mr.address, Port(workerSuffix + strconv.Itoa(i)), MapFunc, ReduceFunc, -1, nil)
+		go RunWorker(mr.address, Port(workerSuffix+strconv.Itoa(i)), MapFunc, ReduceFunc, -1, nil)
 	}
 	mr.Wait()
 	Check(t, mr.Files, jobName)
 	CheckWorker(t, mr.stats)
 	Cleanup(mr)
+}
+
+func TestParallelCheck(t *testing.T) {
+	mr := SetUpParallel()
+	parallelism := &Parallelism{}
+	for i := 0; i < 2; i++ {
+		go RunWorker(mr.address, Port(workerSuffix+strconv.Itoa(i)), MapFunc, ReduceFunc, -1, parallelism)
+	}
+	mr.Wait()
+	Check(t, mr.Files, jobName)
+	CheckWorker(t, mr.stats)
+
+	parallelism.mu.Lock()
+	if parallelism.max < 2 {
+		t.Fatalf("Workers did not execute in parallel")
+	}
+	parallelism.mu.Unlock()
+
+	Cleanup(mr)
+}
+
+func TestOneFailure(t *testing.T) {
+	mr := SetUpParallel()
+	// Start 2 workers that fail after 10 tasks
+	go RunWorker(mr.address, Port(workerSuffix+strconv.Itoa(0)),
+		MapFunc, ReduceFunc, 10, nil)
+	go RunWorker(mr.address, Port(workerSuffix+strconv.Itoa(1)),
+		MapFunc, ReduceFunc, -1, nil)
+	mr.Wait()
+	Check(t, mr.Files, jobName)
+	CheckWorker(t, mr.stats)
+	Cleanup(mr)
+}
+
+func TestManyFailures(t *testing.T) {
+	mr := SetUpParallel()
+	i := 0
+	done := false
+	for !done {
+		select {
+		case done = <-mr.doneChannel:
+			Check(t, mr.Files, jobName)
+			Cleanup(mr)
+			break
+		default:
+			// Start 2 workers each sec. The workers fail after 10 tasks
+			w := Port(workerSuffix + strconv.Itoa(i))
+			go RunWorker(mr.address, w, MapFunc, ReduceFunc, 10, nil)
+			i++
+			w = Port(workerSuffix + strconv.Itoa(i))
+			go RunWorker(mr.address, w, MapFunc, ReduceFunc, 10, nil)
+			i++
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
